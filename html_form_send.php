ceb428e2487155a6b4d589137e3c1fcceaf214bb
JakeEllis <jmelli12@gmail.com>
2016-01-21 22:44:21 -0500

    Update html_form_send.php

--- html_form_send.php
+++ html_form_send.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['email'])) {
+if(isset($_GET['email'])) {
      
     $email_to = "ejpersonalblog@gmail.com";
      
@@ -16,15 +16,15 @@ if(isset($_POST['email'])) {
     }
      
     // validation expected data exists
-    if(!isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message'])) {
+    if(!isset($_GET['name']) ||
+        !isset($_GET['email']) ||
+        !isset($_GET['message'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
      
-    $name = $_POST['name']; // required
-    $email_from = $_POST['email']; // required
-    $comments = $_POST['message']; // required
+    $name = $_GET['name']; // required
+    $email_from = $_GET['email']; // required
+    $comments = $_GET['message']; // required
      
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
