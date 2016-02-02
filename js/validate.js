93e5879a7150bb1f07b3e340ca8e719994b83fef
Office to Adventure <officetoadventure@gmail.com>
2016-02-02 14:07:41 -0500

    Update validate.js

--- js/validate.js
+++ js/validate.js
@@ -1,29 +1,33 @@
 $(document).ready(function () {
      
-            $('#contactform').validate({ // initialize the plugin
-          rules: {
-              name: {
-                  required: true,
-              },
-              email: {
-                  required: true,
-                  email: true
-              },
-              message: {
-                  required: true
-              }
-           },
-           messages: {
-              name: "Please enter your name.",
-              email: "Please enter a valid email address.",
-              message: "Please enter a message."
-               }
-            },
-           submitHandler: function(form) { 
-            $(".submit").attr("disabled", true);
-            form.submit();
-           }
-       });
+            $("#submit").attr('disabled', 'disabled');
+               $("contactform").keyup(function() {
+               // To Disable Submit Button
+          $("#submit").attr('disabled', 'disabled');
+          // Validating Fields
+               var name = $("#name").val();
+               var email = $("#email").val();
+               var message = $("#message").val();
+               var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+          if (!(name == "" || email == "" || message == "")) {
+               if (filter.test(email)) {
+               // To Enable Submit Button
+               $("#submit").removeAttr('disabled');
+               $("#submit").css({
+               "cursor": "pointer",
+               "box-shadow": "1px 0px 6px #333"
+               });
+          }
+          }
+          });
+          // On Click Of Submit Button
+          $("#submit").click(function() {
+          $("#submit").css({
+"cursor": "default",
+"box-shadow": "none"
+});
+alert("Form Submitted Successfully..!!");
+});
      
      
      // var name = false;
